789c5f0435776eb95acadf162345f78bec2fdf17
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:16:40 -0300

    Create operadoreslogicos.js

---
+++ AulasUdemy/aula22/operadoreslogicos.js
@@ -0,0 +1,18 @@
+/*
+Operadores lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU ->   
+! -> NOT -> NÃO
+
+const expressaoAnd = true && true && true && true;
+const expressaoOr = false || true;
+console.log(expressaoAnd);
+console.log(expressaoOr);
+
+const usuario = 'Denis';
+const senha = '12345';
+
+const vaiLogar = usuario ==='Denis' && senha === '12345';
+console.log(vaiLogar);
+*/
+console.log(!(true))
