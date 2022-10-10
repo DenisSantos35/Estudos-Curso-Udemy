8c096863102b696af6abac00953451503add0a3a
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:21:51 -0300

    Create comentarios.js

---
+++ AulasUdemy/aula6/aula4/comentarios.js
@@ -0,0 +1,10 @@
+// e escrever um comentario 
+console.log('Ola mundo'); // Aqui tem outro comentario
+console.log('linha 1')
+/* aqui vai um
+bloco de comentario
+por quantas linhas for necessario*/
+//console.log('linha 2')
+console.log('linha 3')
+console.log('linha 4')
+//isso vai ser ignorado ao ser executado
