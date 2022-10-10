fcc39ee5d4a45f7ebdcccf5080a82af7312a9589
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:22:00 -0300

    Create index.js

---
+++ AulasUdemy/aula6/index.js
@@ -0,0 +1,21 @@
+/*let nome = 'Denis'; // string
+// nao podemos criar variaveis com palavras reservadas
+//variaveis precisa ter nomes significaivos
+//nao podemos comecar  o nome de uma variavel com numero
+//nao podemos conter espacos ou tracos 
+//utilizameos camelCase para declarar variaveis compostas
+//Case-sensitive js e sensivel a caixa
+//Não podemos redeclarar variaveis com let
+//NAO UTILIZE VAR, UTILIZE LET
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+console.log('O Filho de', nome, 'se chama Eduardo. ');*/
+
+let nome; //declara variavel sem valor(undefined)
+nome = 'Joao'; //inicializa a variavel atribuindo valor
+console.log(nome)
+nome = 'Otavio'
+console.log(nome)
