44e4628fa016af4e7f0f42c12cee123953342b2a
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:19:29 -0300

    Create forClassico.js

---
+++ AulasUdemy/aula33/forClassico.js
@@ -0,0 +1,18 @@
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+/*for(let i = 400; i <= 500; i++){
+    const par = i % 2 == 0 ? 'é par': 'é impar'
+    console.log(`${i} ${par}`)
+}*/
+
+//                 0       1      2        array com 3 elementos
+const frutas = ['maca', 'pera', 'uva', 'melancia', 'banana', 'melao']
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(i, frutas[i])
+}
+
