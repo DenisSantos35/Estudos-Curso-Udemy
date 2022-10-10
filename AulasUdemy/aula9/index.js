b02f5b43aefb3a2c3cf83aaf17d3b3527653fde3
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:22:26 -0300

    Create index.js

---
+++ AulasUdemy/aula9/index.js
@@ -0,0 +1,5 @@
+//ECMAScript 2015 (ES6) padronização da linguagem Js
+//var nome = 'Luiz';
+//var nome = 'marcos'
+nome = 'Luiz'; // Não faça isso, cria variavel global podendo afetar o sistema
+console.log(nome);
