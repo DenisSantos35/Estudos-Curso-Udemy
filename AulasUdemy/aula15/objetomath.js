eb0555311c12522232bd31c3b3eb38d354e81493
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:14:43 -0300

    Create objetomath.js

---
+++ AulasUdemy/aula15/objetomath.js
@@ -0,0 +1,22 @@
+let num1 = 9.49;
+let valores = [2,5,8,23,50,-2]
+let arredondarBaixo = Math.floor(num1) // arredonda numero para baixo
+let arredondarCima = Math.ceil(num1) // arredonda numero para cima
+let arrendondarMatematico = Math.round(num1) // arrendondamento por regra maatematica
+console.log(arredondarBaixo)
+console.log(arredondarCima)
+console.log(arrendondarMatematico)
+console.log(Math.max(2,3,5,20)) //retorna o maior valor na comparação
+console.log(Math.min(2,3,5,20)) //retorna o menor valor na comparação
+console.log(Math.random()) //retorna numero aleatorio de 0 a 1 excluindo o 1
+console.log(Math.round(Math.random()*10))
+let max = 5
+let min = 5
+const aleatorio = Math.round(Math.random() * (max) + min); //arredonda numero entre 5 e 10
+console.log(aleatorio)
+console.log(Math.pow(2, 10)) //biblioteca para potenciação 
+console.log(Math.sqrt(5)) //biblioteca para raiz quadrada ou
+console.log(max ** (1/2))
+console.log(100 / 0) //retorna um numero infinito erro grave em js
+
+
