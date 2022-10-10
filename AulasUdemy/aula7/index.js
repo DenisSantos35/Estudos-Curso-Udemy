e7b72f6952d9c71a9d6289a1ddc5ac3f6cff0b16
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:22:08 -0300

    Create index.js

---
+++ AulasUdemy/aula7/index.js
@@ -0,0 +1,21 @@
+// nao podemos criar CONSTANTES com palavras reservadas
+//CONSTANTES precisa ter nomes significaivos
+//nao podemos comecar  o nome de uma CONSTANTE com numero
+//nao podemos conter espacos ou tracos 
+//utilizameos camelCase para declarar CONSTANTES compostas
+//Case-sensitive js e sensivel a caixa
+//Não podemos redeclarar CONSTANTES com const
+//constantes sao imutaveis
+// string - text | Number = Número
+const nome = 'Joao'
+console.log(nome)
+const primeiroNumero = 5; //Number
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2
+let resultadoTriplicado = resultado * 3
+resultadoTriplicado = resultadoTriplicado + 5
+console.log(resultado);
+console.log(resultadoDuplicado)
+console.log(resultadoTriplicado)
+console.log(typeof(primeiroNumero))
