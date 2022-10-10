a505843621bf285ab6472bc2ec6c7afa9c540153
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:18:52 -0300

    Create index.js

---
+++ AulasUdemy/aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Denis "Santos"'); //entre aspas sao considerados strings = texto
+console.log("Denis 'Santos'");
+console.log(`'Denis' "Santos"`);
+
+console.log(10, 15.85, 'Denis Santos'); //fora das aspas sao considerados numbers = numeros
