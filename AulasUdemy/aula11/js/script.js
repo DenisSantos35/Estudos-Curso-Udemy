53f1e129fdcd4c8a3cc2e3c93f6510f8335ef86e
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:13:31 -0300

    Create script.js

---
+++ AulasUdemy/aula11/js/script.js
@@ -0,0 +1,8 @@
+
+const num1 = Number(prompt('Digite um valor: '))
+const num2 = Number(prompt('Digite outro valor:'))
+let soma = num1 + num2
+
+alert(`O valor da soma de ${num1} e ${num2} e igual a ${soma}`)
+    
+
