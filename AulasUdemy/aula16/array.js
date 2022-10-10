1ad9bd2d2b429cd181f3efc44b7c6339c694acbd
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:14:53 -0300

    Create array.js

---
+++ AulasUdemy/aula16/array.js
@@ -0,0 +1,43 @@
+const aluno = ['Luiz', 'Maria', 'Joao'];
+console.log(typeof aluno)
+console.log(aluno instanceof Array)
+
+//aluno.push('Luiza', 'Eduardo')
+//console.log(aluno[4])
+//console.log(aluno.slice(-2,-1)) // fateamento de traz 
+//console.log(aluno.slice(3,5))  //fateamento de frente
+
+
+/*console.log(aluno[50])
+if (aluno[50] == undefined){
+    console.log('verdadeiro')
+} */
+
+//delete aluno[1];
+//console.log(aluno);
+
+
+//const removeFim = aluno.pop() // remove do final
+//const removeIni = aluno.shift() //remove inicio
+//aluno.unshift('Luiza') //adiciona inicio
+//aluno.push('Marta') //adiciona final
+
+//console.log(removeFim) //mostra o nome removido no final
+//console.log(removeIni) //mostra o nome removido inicio
+//console.log(aluno)
+//aluno[aluno.length] = 'Denis'  //adiciona no fim
+//aluno[aluno.length] = 'Fábio'  //adiciona no fim
+//aluno[aluno.length] = 'Joyce'  //adiciona no fim
+
+//aluno.push('Istefany') //adiciona no fim
+
+/*console.log(aluno)
+console.log(aluno[0]) //mostra nome no array na poicao
+console.log(aluno[1])
+console.log(aluno[2])
+console.log(`Aluno joao esta na posição ${aluno.indexOf('Joao')}`) //mostra a posicao do nome
+aluno[0] = 'Marcos'
+console.log(aluno)
+aluno[3] = 'Luiza'
+console.log(aluno)
+console.log(aluno.length)*/
