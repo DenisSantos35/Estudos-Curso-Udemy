1f8136843374e4e6fcdf8f23cf949bf6fab4c329
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:11:47 -0300

    Operadores Atitimeticos

---
+++ AulasUdemy/aula10/operadoresAritimeticos.js
@@ -0,0 +1,27 @@
+/* 
+--> Aritimeticos
++ adição
+- subtração e concatenação
+* multiplicação
+/ divisão
+** potenciação
+% mod resto da divisao
+ordem de precedencia
+1 -> ()
+2 -> **
+3 -> / * %
+4 -> + -
+NaN - not a Number -> nao e um numero
+parseInt -> comverte para numero inteiro
+parseFloat -> converte para numero flutuante
+Number -> converte para numero sem distinção
+
+*/
+const num = parseInt('5');
+const num1 = parseFloat('5.3')
+const num2 = 10;
+const num3 = Number('3.6')
+console.log(num + num2 + num1)
+console.log(typeof(num))
+console.log(typeof(num1))
+
