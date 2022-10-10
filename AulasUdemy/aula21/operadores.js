11fd84846134fedf4f2a719a4ce4cd82c792c1aa
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:16:22 -0300

    Create operadores.js

---
+++ AulasUdemy/aula21/operadores.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual
+< menor que 
+<= menor ou igual a
+== igualdade (valor) **********
+=== igualdade estrita (cheva valor e tipo)
+!= diferente *********
+!== diferente estrita(valor e tipo)
+*/
+const num1 = 10; //number
+const num2 = 10; //string
+
+const expressão = num1 !== num2;
+console.log(expressão);
+
