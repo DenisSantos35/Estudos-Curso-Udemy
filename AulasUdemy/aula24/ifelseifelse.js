82fe1f989bf9478f581f3f588416ebc7bba0aa44
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:16:57 -0300

    Create ifelseifelse.js

---
+++ AulasUdemy/aula24/ifelseifelse.js
@@ -0,0 +1,11 @@
+const hora = 25;
+
+if (hora >=0 && hora <= 11){
+    console.log('Bom dia');
+}else if (hora <= 17){
+    console.log('Boa Tarde')
+}else if(hora <= 23){
+    console.log('Boa noite')
+}else{
+    console.log('ola')
+}
