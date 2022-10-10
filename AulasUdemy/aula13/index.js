fdc49f6c374664623d964112339088dcce1cfd9f
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-09 21:14:04 -0300

    Create index.js

---
+++ AulasUdemy/aula13/index.js
@@ -0,0 +1,34 @@
+/*let umaString = "Um \"texto\""
+console.log(umaString)
+console.log(umaString[5])
+console.log(umaString.charAt(6))
+
+console.log(umaString.length)
+
+umaString = "Um texto"
+console.log(`${umaString} valor contido em umaString`)
+console.log(`${umaString[6]} valor contido na posição 6 de umaString`)
+console.log(`${umaString.indexOf('texto')} verifica se tem texto dentro da string e 
+devolve a posição inicial da palavra dentro da string`)
+console.log(`${umaString.lastIndexOf('t')} verifica a posição da letra dentro da 
+string começando de traz para frente`)
+console.log(umaString.indexOf('t'))  // apartir da posicao 3 ache a letra o
+console.log(umaString.indexOf('t '))
+console.log(umaString.length) //verifica o tamanho da string
+familia = ['aves', 'peixes']
+console.log(familia)
+console.log(umaString.match(/[a-z]/g)) //dentro da estring retorna um array com as
+                                        //letras minusculas
+console.log(umaString.match(/[A-Z]/g))//dentro da estring retorna um array com as
+                                    //letras maiuscul
+console.log(umaString.search(/[A-Z]/g))//retorna a posição da primeira letra maiuscula
+console.log(umaString.replace(" ", ","))*/
+let frase = 'O rato roeu a roupa do rei de roma'
+/*console.log(frase.replace(/r/g, "#")) //para trocar todos os r por # usa  g para trocar
+// todos os r ou sem o g troca so o primeiro
+
+console.log(frase.slice(2,6))
+console.log(frase.slice(-16))
+console.log(frase.slice(-5))
+console.log(frase.slice(-8,-5))*/
+console.log(frase.split('r'));
