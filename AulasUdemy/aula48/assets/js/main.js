4f1a580b185b308d1f6328b6536e56cc916b6ce7
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-10-28 21:26:09 -0300

    ListaDeTarefas
    Nesta lista utilizamos varias funcoes bem como vario elementos de JS para fazer uma lista iteravel e que nao se apaga caso feche o navegador

--- AulasUdemy/aula48/assets/js/main.js
+++ AulasUdemy/aula48/assets/js/main.js
@@ -13,11 +13,23 @@ inputTarefa.addEventListener('keypress', function(e){ //função para pegar tecl
         criaTarefa(inputTarefa.value);  // chama funcao para criar a frase e passa o valor que esta no imput
     }
 } );
+function getButton(li){ //ele vai ser criado no li
+    li.innerText += ' '; //da um espaco entre o botao e o texto dentro da li
+    const botao = document.createElement('button'); //crea o elemento botao
+    botao.innerText = 'Apagar'; //coloca nome no elemento botao com apagar
+    //botao.classList.add('botao');
+    botao.setAttribute('class', 'apagar');
+    botao.setAttribute('title', 'Apagar tarefa');    
+    li.appendChild(botao); //o boatao e filho de li
+    
+}
 function criaTarefa(msg){  //funcao para criar mensagem da tarefa 
       const li = getLi(); //chama funcao que cria a linha da ul
       li.innerText = msg; // armazena na ul a linha com a mensagem
+      getButton(li); //chama a funcao botao e passa o valor dentro do li
       tabela.appendChild(li); 
       getLimpa();   //apos criado chama funcao para apagar o imput
+      salvarTarefas();
 }
 
 function getLimpa(){  //funcao para limpar o imput e deixar o cursor piscando para nova tarefa
@@ -32,3 +44,38 @@ adicionarTarefa.addEventListener('click', function(){ //adicona um monitoramento
     criaTarefa(inputTarefa.value); //funçao para elaborar a mensagem no console
 
 });
+
+document.addEventListener('click', function(e){ //monitora todo o hmle e onde clicar pega o elemento e armazee no (e);
+    const evento = e.target; //atribui ao evento o elemento clicado
+    console.log(evento);
+    if(evento.classList.contains('apagar')){ //se o evento contem a classe listada 'apagar'; se for verdadeiro realiza o que esta abaixo.
+        evento.parentElement.remove(); //procura o pai do evento e remove ele por completo.
+        salvarTarefas();
+    }
+
+});
+
+function salvarTarefas(){
+    const liTarefas = tabela.querySelectorAll('li'); //seleciona todos os elementos que estao dentro da ul(tabela)
+    const listaDeTarefa = []; //lista para cebeber valores
+    for(let tarefa of liTarefas){  //passa os valores que escao atribuidos a constante liTarefas
+        let texto = tarefa.innerText; //atribui a texto cada palavra que passa por tarefa, capiturando so o texto.
+        texto = texto.replace('Apagar', '').trim(); //replace retira o texto apagar e deixa apenas  nome
+        //trim faz o mesmo que o splice do python removedo os espacos
+        listaDeTarefa.push(texto);
+        console.log(listaDeTarefa);
+    }
+    //json salva dados entre sistemas
+    const tarefaJSON = JSON.stringify(listaDeTarefa); //faz o array converter para string e convertida para JSON.
+    localStorage.setItem('tarefas', tarefaJSON); // salva no local storage do html com key tarefas e value tarefaJSON ou seja aramazna em value o array convertido em stringJSON
+}
+function adicionaTarefasSalvas(){ // funcao para manter salvo as tarefas
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefa = JSON.parse(tarefas,)
+    console.log(listaDeTarefa);
+    for(let tarefa of listaDeTarefa){
+        criaTarefa(tarefa);
+    }
+}
+
+adicionaTarefasSalvas();
