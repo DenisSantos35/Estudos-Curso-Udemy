c3d26f55aa6514d31cfa8c2ea84375500423ca58
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-12-03 21:43:58 -0300

    Update exercicioCpf.js

--- AulasUdemy/aula77/assets/js/exercicioCpf.js
+++ AulasUdemy/aula77/assets/js/exercicioCpf.js
@@ -14,40 +14,74 @@
 
 Se o numero for maior que 9, consideramos 0
 */
+// *********************************************captura elementos html*****************************************
+const cpf =  document.querySelector('.cpf');
+const btn = document.querySelector('.botao');
+const resposta = document.querySelector('.res')
+// ************************************declaracao variavel para recebimento cpf********************************
+let cpfValido;
+// let cpf = '070.987.720'; //recebendo cpf
 
-let cpf = '383.716.198-63'; //recebendo cpf
-let cpfLimpo = cpf.replace(/\D+/g, '') //exprecao regular tudo que nao for numero substitui por nada
-let lista = Array.from(cpfLimpo)
-let contador = 10;
-let cpfValido = '';
+// ****************************************validacao 1º digito do cpf*******************************************
+function cpfParte1(){
+    let cpfLimpo = cpf.value.replace(/\D+/g, '') //exprecao regular tudo que nao for numero substitui por nada
+    console.log(cpfLimpo);
+    let lista = Array.from(cpfLimpo); //trnasforma o cpf limpo em array
+    return cpfValido = validacaoCpf(lista, 10); // chama funcao de validacao de digito e passa como parametro o array dentro dalista e o valor do contador para validar o 1º digito.
+}
 
-function validacaoCpf(lista){
+// **********************************************validacao 2º digito cpf******************************************
+function cpfParte2(){
+    const lista2  = Array.from(cpfParte1()); //pega os valores da validacao do 1º digito e transforma em um novo array e atribui a lista 2
+    return cpfValido = validacaoCpf(lista2,11); //chama a funcao validadora de digito passando a lista 2 e o contador igual a 11
+}
+
+// **********************************************validacao do 1º e 2º digito para cpf**********************************
+function validacaoCpf(lista, cont){
+    let contador = cont; //inicia o contador com valor 10 para 1º e 11 para 2° digitos
+    let cpfValido = ''; //limpa campo cpfValido
+    //junta o calculo em um array atraves de reduce utilizando o acumulador para fazer a somatoria
     lista = lista.reduce((acumulador, valor) => { 
-        while(contador >= 2) { 
+        //enquanto o contador for maior ou igual a 2 faz os calculos, decrementa o contador e concatena os valores em valor
+        while(contador >= 2) {  
             acumulador += Number(valor) * contador;
             contador -= 1;   
             cpfValido += valor;
-            return acumulador
+            return acumulador //retorna para que de continuação no calbeck realizado pelo acumulador do reduce
         }
-        return acumulador
+        return acumulador //caso o contador seja maior nao acumula mais e retorna o valor 
     },0);
+    //calculo para achar o digito caso seja maior que 9 atribui que vale 0
     lista = 11 - (lista % 11)
     if(lista > 9){
         lista = 0;
+    }    
+    return cpfValido += lista //retorna a concatenacao incrementado dos digitos 1 e 2 da verificacao
+}
+
+// *****************************************Evento de click do botao **********************************
+btn.addEventListener('click', () =>{
+    cpfParte1(); //ao clicar em enviar chama a primeira pate de validacao do cpf
+    const validado = cpfParte2(); //chama a segunda parte de validacao e retorna o valor para a constante validado
+    let cpfLimpo = cpf.value.replace(/\D+/g, ''); //transforma em um valor sem pontos e tracos
+
+    //Condicao que compara se o numero digitado e valido após a verificação
+    //comparação é feita pelo valor que foi retornado da funcao cpfPate2() e o cpfValidado
+    if(cpfLimpo === validado){
+        console.log(`O CPF: ${cpf.value} é valido`)
+        resposta.innerHTML = `O CPF: ${cpf.value} é valido`
+
+        
+    }else{
+        console.log(`O CPF: ${cpf.value} é invalido`)
+        resposta.innerHTML = `O CPF: ${cpf.value} é invalido`    
     }
-    console.log(lista)
-    return cpfValido += lista 
-}
-
-cpfValido = validacaoCpf(lista);
-contador = 11;
-const lista2  = Array.from(cpfValido);
-cpfValido = '';
-cpfValido = validacaoCpf(lista2);
+    cpf.value = '';
+    cpf.focus();
+})
+/*
 
 
-if(cpfLimpo === cpfValido){
-    console.log(`O CPF: ${cpf} é valido`)
-}else{
-    console.log(`O CPF: ${cpf} é invalido`)
-}
+
+
+*/
